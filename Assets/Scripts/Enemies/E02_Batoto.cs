87563d7b4626ade8bb9bc4e67ad234df236525e1
Takoyakiisgood <156163665+Tako27@users.noreply.github.com>
2024-07-17 19:50:07 +0800

    create E02

--- Assets/Scripts/Enemies/E02_Batoto.cs
+++ Assets/Scripts/Enemies/E02_Batoto.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime.Misc;
+
 using UnityEngine;
 
-public class E01_Ratoto : EnemyController
+public class E02_Batoto : EnemyController
 {
     protected override void EnemyAttack()
     {
