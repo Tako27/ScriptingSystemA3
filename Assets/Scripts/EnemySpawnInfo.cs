676c53de9a3711313c7e4f46d731870749a505ef
Takoyakiisgood <156163665+Tako27@users.noreply.github.com>
2024-07-18 10:19:23 +0800

    commented another code

--- Assets/Scripts/EnemySpawnInfo.cs
+++ Assets/Scripts/EnemySpawnInfo.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Code Done By: Celest Goh Zi Xuan
+// ================================
+// This script holds the configuration for spawning enemies
+// in waves within a specific map.
+
 public class EnemySpawnInfo
 {
     public string mapID;
