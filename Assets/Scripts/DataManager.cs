67c8f1dfcf87ca41fab5682a3cebbf19a64fc75b
yxxxj <leeyingjieok@gmail.com>
2024-07-29 09:52:35 +0800

    fix dialogue

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -416,8 +416,14 @@ public class DataManager : MonoBehaviour
             refDialogue.dialogueType = columnData[4];
             refDialogue.typeID = columnData[5];
 
-            PlayerDialogue dialogue = new PlayerDialogue(refDialogue.triggerID, refDialogue.id, refDialogue.dialogue, refDialogue.dialogueBy, refDialogue.dialogueType, refDialogue.typeID);
-            playerDialoguesList.Add(dialogue);
+
+            string[] triggerIDs = refDialogue.triggerID.Split("@");
+            foreach(string triggerID in triggerIDs)
+            {
+                PlayerDialogue dialogue = new PlayerDialogue(triggerID.Trim(), refDialogue.id, refDialogue.dialogue, refDialogue.dialogueBy, refDialogue.dialogueType, refDialogue.typeID);
+                playerDialoguesList.Add(dialogue);
+            }
+
             //add all initialized player dialogue data into this list
             //this list acts as the main list of player dialogues, which contains all available plaer dialogue in game
             //which will be referenced by the game script to set the player dialogue list in game
